f70cd98cb114153549194f6e49347502cf0906a8
Iva Maric <iva.maric@flabbergast.agency>
2022-12-30 13:00:11 +0100

    deploy fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path';
 import fs from "fs";
 import fse from 'fs-extra';
 import webpush from "web-push";
+import cors from 'cors';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -14,10 +15,11 @@ const upload = path.join(__dirname, "public", "uploads");
 const subscription = 'subscriptions.json';
 let subscriptions = [];
 
-const httpPort = 80
+const httpPort = 8080
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(httpPort, function () {
